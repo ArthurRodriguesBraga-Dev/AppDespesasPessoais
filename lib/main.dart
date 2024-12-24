305b74f96a55be53d49c5c22535020bb482ea0da
Arthur Rodrigues <arthurrodriguesestudante@gmail.com>
2024-12-24 10:41:00 -0300

    Adicionando os Cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
 
-main()=> runApp(ExpensesApp());
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage(),);
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
   }
 }
-
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Despesas Pessoais"),), body: Center(child: Text("Versão Inicial"),),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas Pessoais"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 10,
+              child: const Text("Gráfico"),
+            ),
+          ),
+          Card(
+            child: Text("Lista de Transações"),
+          )
+        ],
+      ),
+    );
   }
 }
