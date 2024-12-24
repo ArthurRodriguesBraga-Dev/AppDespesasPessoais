dd2edfa11b606a32427513486177e609dbe0f008
Arthur Rodrigues <arthurrodriguesestudante@gmail.com>
2024-12-24 10:08:31 -0300

    Primeiro Commit e início do projeto do App despesas pessoais

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+main()=> runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage(),);
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Despesas Pessoais"),), body: Center(child: Text("Versão Inicial"),),);
+  }
+}
